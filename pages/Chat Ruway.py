5a0c7ee5331ab77d93b0ece7b5816f80d70c07af
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-11-22 11:09:16 -0500

    Update Chat Ruway.py

--- pages/Chat Ruway.py
+++ pages/Chat Ruway.py
@@ -8,38 +8,55 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
+import PyPDF2
 
-#import pickle5 as pickle
-#from pathlib import Path
+import glob
+from gtts import gTTS
+import os
+import time
 
-st.title('Aprende sobre Ruway 💬')
+try:
+    os.mkdir("temp")
+except:
+    pass
+
+
+
+st.title('Que deseas Saber de IM ? 💬')
 ke = st.text_input('Ingresa tu Clave')
 #os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 os.environ['OPENAI_API_KEY'] = ke
 
+pdfFileObj = open('example.pdf', 'rb')
+ 
+# creating a pdf reader object
+pdfReader = PyPDF2.PdfReader(pdfFileObj)
+
+
     # upload file
-pdf = st.file_uploader("Carga el archivo PDF", type="pdf")
+#pdf = st.file_uploader("Carga el archivo PDF", type="pdf")
 
    # extract the text
-if pdf is not None:
-      from langchain.text_splitter import CharacterTextSplitter
-      pdf_reader = PdfReader(pdf)
-      text = ""
-      for page in pdf_reader.pages:
+#if pdf is not None:
+from langchain.text_splitter import CharacterTextSplitter
+ #pdf_reader = PdfReader(pdf)
+pdf_reader  = PyPDF2.PdfReader(pdfFileObj)
+text = ""
+for page in pdf_reader.pages:
          text += page.extract_text()
 
    # split into chunks
-      text_splitter = CharacterTextSplitter(separator="\n",chunk_size=500,chunk_overlap=20,length_function=len)
-      chunks = text_splitter.split_text(text)
+text_splitter = CharacterTextSplitter(separator="\n",chunk_size=500,chunk_overlap=20,length_function=len)
+chunks = text_splitter.split_text(text)
 
 # create embeddings
-      embeddings = OpenAIEmbeddings()
-      knowledge_base = FAISS.from_texts(chunks, embeddings)
+embeddings = OpenAIEmbeddings()
+knowledge_base = FAISS.from_texts(chunks, embeddings)
 
 # show user input
-      st.subheader("Escribe que quieres saber sobre Ruway")
-      user_question = st.text_input(" ")
-      if user_question:
+st.subheader("Escribe que quieres saber sobre el documento")
+user_question = st.text_input(" ")
+if user_question:
         docs = knowledge_base.similarity_search(user_question)
 
         llm = OpenAI()
@@ -48,3 +65,37 @@ if pdf is not None:
           response = chain.run(input_documents=docs, question=user_question)
           print(cb)
         st.write(response)
+
+        def text_to_speech(text, tld):
+                
+                tts = gTTS(response,"es", tld , slow=False)
+                try:
+                    my_file_name = text[0:20]
+                except:
+                    my_file_name = "audio"
+                tts.save(f"temp/{my_file_name}.mp3")
+                return my_file_name, text
+
+    
+        if st.button("Escuchar"):
+          result, output_text = text_to_speech(response, 'es')
+          audio_file = open(f"temp/{result}.mp3", "rb")
+          audio_bytes = audio_file.read()
+          st.markdown(f"## Escucha:")
+          st.audio(audio_bytes, format="audio/mp3", start_time=0)
+
+
+
+            
+          def remove_files(n):
+                mp3_files = glob.glob("temp/*mp3")
+                if len(mp3_files) != 0:
+                    now = time.time()
+                    n_days = n * 86400
+                    for f in mp3_files:
+                        if os.stat(f).st_mtime < now - n_days:
+                            os.remove(f)
+                            print("Deleted ", f)
+            
+            
+          remove_files(7)
