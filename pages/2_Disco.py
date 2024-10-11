72d20a1f7b8f350a991fcb092d2a2d5c673eb78a
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-10-11 14:10:46 -0500

    Update 2_Disco.py

--- pages/2_Disco.py
+++ pages/2_Disco.py
@@ -3,6 +3,7 @@ import time
 import streamlit as st
 from PIL import Image
 import json
+from streamlit_drawable_canvas import st_canvas
 act1="OFF"
 
 def on_publish(client,userdata,result):             #create function for callback
@@ -25,7 +26,38 @@ client1.on_message = on_message
 
 st.title("El DISCO DE LA ENCARNACIÓN.")
 
-#st.subheader("PLEGARIAS")
+with st.sidebar:
+    st.subheader("Acerca de:")
+    st.subheader("En esta aplicación veremos la capacidad que ahora tiene una máquina de interpretar un boceto")
+    bg_image = st.sidebar.file_uploader("Cargar Imagen:", type=["png", "jpg"])
+    bg_color = st.color_picker("Color de Fondo", "#FFFFFF",key="2")
+    stroke_color = st.color_picker("Color de Trazo", "#000000",key="1")
+    stroke_width = st.sidebar.slider('Selecciona el ancho de línea', 1, 30, 5,key="A")
+    #H_= st.sidebar.slider('Selecciona la altura del tablero ', 1, 400, 10,key="B")
+    #W_= st.sidebar.slider('Selecciona el ancho del tablero', 1, 400, 10,key="C")
+    
+st.subheader("Dibuja el boceto en el panel  y presiona el botón para analizarla")
+
+
+#realtime_update = st.sidebar.checkbox("Update in realtime", True)
+drawing_mode = st.sidebar.selectbox(
+    "Herramienta de dibujo:",
+    ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
+  )
+
+# Create a canvas component
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    background_image=Image.open(bg_image) if bg_image else None,
+    height=400,
+    width=400,
+    drawing_mode=drawing_mode,
+    key="canvas",
+)
+
 
 
 image = Image.open('Invoc.png')
