3510bf6cbae845ad0eef73e1b44ac60c23dd01b1
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-11-29 08:04:37 -0500

    Update Oráculo.py

--- pages/Oráculo.py
+++ pages/Oráculo.py
@@ -68,8 +68,8 @@ try:
     knowledge_base = FAISS.from_texts(chunks, embeddings)
     
     # show user input
-    st.subheader("Has sido autorizado, Realiza la pregunta")
-    user_question = st.text_input(" ")
+    st.subheader("Realiza tú pregunta")
+    user_question = st.text_area(" ")
     if user_question:
             docs = knowledge_base.similarity_search(user_question)
 
